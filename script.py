74a704aaabeb2092424243b151d1b897ae94c493
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-09-17 11:10:57 +0600

    Update script.py

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import undetected_chromedriver.v2 as uc
 from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
+from random import randint as rand
 
 def clear_input_box(driver):
 	driver.find_element_by_xpath('//input[@autocapitalize]').clear()
@@ -10,34 +11,33 @@ def search_keyword(driver, Keyword):
 	driver.find_element_by_xpath('//input[@autocapitalize]').send_keys(Keyword)
 
 def get_result(driver, key:str):
-	post_number_xpath = f'//input[@autocapitalize]/following-sibling::div//div[text()="{key}" and text()="#"]/../../..//span[text()=" posts"]/span'
+	post_number_xpath = f'//input[@autocapitalize]/following-sibling::div//div[text()="{key}" and text()="#"]/../../..//span[text()=" posts" or text()=" post"]/span'
 	try:
 		return driver.find_element_by_xpath(post_number_xpath).text
 	except:
 		return None
 
-options = uc.ChromeOptions()
-options.user_data_dir = "chrome_profile"
-options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
-driver = uc.Chrome(options=options)
-driver.get('https://instagram.com')
+driver = uc.Chrome()
+driver.get('https://www.instagram.com/accounts/login/')
 
 input('Log in and press enter to continue... ')
 
 keys = [i['keys'] for i in pd.read_csv('import.csv').to_dict('records')]
 
 d = []
-i=1
-for key in keys:
-	clear_input_box(driver)
-	search_keyword(driver, key)
-	time.sleep(3)
-	result = get_result(driver, key)
-	print(f"{i} - {key} - {result}")
-	d.append({
-		'time':time.ctime(),
-		'keyword':key,
-		'result':result
-		})
-	i+=1
-pd.DataFrame(d).to_csv('export.csv')
+try:
+	i=1
+	for key in keys:
+		clear_input_box(driver)
+		search_keyword(driver, key)
+		time.sleep(rand(5,7))
+		result = get_result(driver, key)
+		print(f"{i} - {key} - {result}")
+		d.append({
+			'time':time.ctime(),
+			'keyword':key,
+			'result':result
+			})
+		i+=1
+finally:
+	pd.DataFrame(d).to_csv('export.csv')
